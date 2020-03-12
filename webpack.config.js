07b3651cb85d3700ac7cc825e6a634daf2ddeff5
Melvin Viana <mjviana057@gmail.com>
2020-03-11 19:41:13 -0700

    Initial setup - dependencies and webpack.config.js (#1)
    * Add dependencies
    
    * webpack.config.js
    
    * Make airbnb the style guide on exlint
    
    * Setup dependencies, eslint, and webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+
+  entry: './client/src/index.jsx',
+  output: {
+    filename: 'build.js',
+    path: path.resolve(__dirname, './client/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
