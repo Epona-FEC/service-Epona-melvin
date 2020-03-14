4506715e219df8c5a896c431764f533f7fc002e0
Melvin Viana <mjviana057@gmail.com>
2020-03-13 17:15:15 -0700

    Database - Review Model (#6) - Add scores to the schema
    * Add score to Review Model

--- db/utils/generateReviews.js
+++ db/utils/generateReviews.js
@@ -7,6 +7,10 @@ for (let i = 0; i < 100; i += 1) {
   const currentReview = {};
   const isShop = (productOrShop() === 1);
   currentReview.body = lorem.paragraph();
+<<<<<<< HEAD
+=======
+  currentReview.score = random.number(5);
+>>>>>>> c5c09e74f84665dc639773f86cbc6210eb06c897
   currentReview.ShopId = (isShop) ? oneToOneHundred() : null;
   currentReview.ProductId = (!isShop) ? oneToOneHundred() : null;
   currentReview.ReviewerId = oneToOneHundred();
