0172cfd7f874235f304fb54e202c062e73f86024
Melvin-viana <mjviana057@gmail.com>
2020-03-18 11:05:59 -0700

    Add see more reviews functionality

--- client/src/components/ReviewContainer.jsx
+++ client/src/components/ReviewContainer.jsx
@@ -10,7 +10,8 @@ class ReviewContainer extends React.Component {
     super(props);
     this.state = {
       productReviews: sampleData,
-      shopReviews: [],
+      seeMoreReviewsIsClicked: false,
+      shopReviews: [1, 2, 3, 4, 5, 7, 8, 20],
       selectedReviews: 'product',
     };
     this.getFirstFourReviews = this.getFirstFourReviews.bind(this);
@@ -33,29 +34,68 @@ class ReviewContainer extends React.Component {
     return firstFour;
   }
 
+  getRestOfReviews(reviews) {
+    const rest = [];
+    for (let i = 4; i < 20; i += 1) {
+      const reviewData = reviews[i];
+      if (reviews[i] === undefined) {
+        break;
+      } else {
+        rest.push(<Review
+          review={reviewData.review}
+          product={reviewData.product}
+          reviewer={reviewData.reviewer}
+        />);
+      }
+    }
+    return rest;
+  }
+
+  moreReviewsClick() {
+    this.setState({ seeMoreReviewsIsClicked: true });
+  }
+
   render() {
     const { productReviewTotal, shopReviewTotal } = this.props;
-    const { productReviews, shopReviews } = this.state;
+    const {
+      productReviews, shopReviews, selectedReviews, seeMoreReviewsIsClicked,
+    } = this.state;
 
     return (
       <div className='review-container'>
         <div className="review-tabs">
-          <button type="button" className="product-review-button" onClick={()=> this.setState({ selectedReviews: 'product' })}>
+          <button type="button" className="product-review-button" onClick={() => this.setState({ selectedReviews: 'product' })}>
             Reviews for this item
-            <span> {productReviewTotal}</span>
+            <span>
+              {productReviewTotal}
+            </span>
           </button>
-          <button type="button" className="shop-review-button" onClick={()=> this.setState({ selectedReviews: 'shop' })}>
+          <button type="button" className="shop-review-button" onClick={() => this.setState({ selectedReviews: 'shop' })}>
             Reviews for this shop
             <span>{shopReviewTotal}</span>
           </button>
         </div>
-
-        {this.getFirstFourReviews(productReviews).map((data) => data)}
-        {(productReviews.length > 4) ? <button className="more-reviews-button" type="button">See More</button> : null}
+        {/* See More Reviews User story TODO: When see more reviews is clicked */}
+        {/* Up to 20 reviews are shown, if less than 20 it will show all of the reviews */}
+        {(selectedReviews === 'product') ? [
+          <Reviews getReviews={this.getFirstFourReviews} reviews={productReviews} />,
+          (productReviews.length > 4 && !seeMoreReviewsIsClicked)
+            ? <button className="more-reviews-button" type="button" onClick={() => this.moreReviewsClick()}>See More</button>
+            : null,
+          (seeMoreReviewsIsClicked)
+            ? <Reviews getReviews={this.getRestOfReviews} reviews={productReviews} /> : null,
+        ] : [
+          <Reviews getReviews={this.getFirstFourReviews} reviews={shopReviews} />,
+          (shopReviews.length > 4 && !seeMoreReviewsIsClicked) ? <button className="more-reviews-button" type="button" onClick={() => this.moreReviewsClick()}>See More</button> : null,
+          (seeMoreReviewsIsClicked)
+            ? <Reviews getReviews={this.getRestOfReviews} reviews={shopReviews} /> : null,
+        ]}
       </div>
     );
   }
 }
+// TODO: Refacor this line to put it in
+const Reviews = ({ getReviews, reviews }) => getReviews(reviews).map((data) => data);
 
 ReviewContainer.propTypes = {
   productReviewTotal: PropTypes.number,
--- client/src/test/ReviewContainer.test.jsx
+++ client/src/test/ReviewContainer.test.jsx
@@ -24,7 +24,7 @@ describe('ReviewContainer', () => {
 
   // Test see more button not to be there when state array is less than 4
   it('See more button is visible if more than 4 reviews', () => {
-    wrapper.setState({productReviews: [1, 2, 3] });
+    wrapper.setState({ productReviews: [1, 2, 3] });
     expect(wrapper.contains(<button className="more-reviews-button" type="button">See More</button>)).toBe(false);
   });
 
@@ -41,10 +41,9 @@ describe('ReviewContainer', () => {
 
   // https://www.etsy.com/listing/737189527/crystal-bridal-headband-crystal-bridal?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=&ref=sc_gallery-1-1&plkey=1d3e3fe8f64cca5ea1d347e588198c8c1651a52e%3A737189527&pro=1&frs=1
 
-  // Has more than four reviews and less than 20, shows the rest of the array when see more is clicked
+  // Has more than 4 reviews and less than 20, shows the rest of the array when see more is clicked
 
-  //https://www.etsy.com/listing/535133523/new-colors-unicorn-horns-indvidual-or?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=&ref=sr_gallery-1-5&bes=1&col=1
+  // https://www.etsy.com/listing/535133523/new-colors-unicorn-horns-indvidual-or?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=&ref=sr_gallery-1-5&bes=1&col=1
 
   // Has more than 20 reviews, when see more is clicked a Read All reviews is shown
-
 });
