a346f833c6ee898478e3d7de3b07fc1c727ea69e
Melvin Viana <mjviana057@gmail.com>
2020-03-18 11:57:53 -0700

    Review container (#16)
    Create review container front end.
    
    Add tests for ReviewContainer
    
    Still need to working on this.

--- client/src/components/ReviewContainer.jsx
+++ client/src/components/ReviewContainer.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable class-methods-use-this */
 import React from 'react';
 import PropTypes from 'prop-types';
+import sampleData from '../../sampleData';
 
 import Review from './Review.jsx';
 
@@ -7,34 +9,93 @@ class ReviewContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      reviewArray: [1, 2, 3, 4],
+      productReviews: sampleData,
+      seeMoreReviewsIsClicked: false,
+      shopReviews: [1, 2, 3, 4, 5, 7, 8, 20],
+      selectedReviews: 'product',
     };
+    this.getFirstFourReviews = this.getFirstFourReviews.bind(this);
+  }
+
+  getFirstFourReviews(reviews) {
+    const firstFour = [];
+    for (let i = 0; i < 4; i += 1) {
+      const reviewData = reviews[i];
+      if (reviews[i] === undefined) {
+        break;
+      } else {
+        firstFour.push(<Review
+          review={reviewData.review}
+          product={reviewData.product}
+          reviewer={reviewData.reviewer}
+        />);
+      }
+    }
+    return firstFour;
+  }
+
+  getRestOfReviews(reviews) {
+    const rest = [];
+    for (let i = 4; i < 20; i += 1) {
+      const reviewData = reviews[i];
+      if (reviews[i] === undefined) {
+        break;
+      } else {
+        rest.push(<Review
+          review={reviewData.review}
+          product={reviewData.product}
+          reviewer={reviewData.reviewer}
+        />);
+      }
+    }
+    return rest;
+  }
+
+  moreReviewsClick() {
+    this.setState({ seeMoreReviewsIsClicked: true });
   }
 
   render() {
     const { productReviewTotal, shopReviewTotal } = this.props;
-    const { reviewArray } = this.state;
+    const {
+      productReviews, shopReviews, selectedReviews, seeMoreReviewsIsClicked,
+    } = this.state;
+
     return (
       <div className='review-container'>
         <div className="review-tabs">
-          <button type="button">
+          <button type="button" className="product-review-button" onClick={() => this.setState({ selectedReviews: 'product' })}>
             Reviews for this item
-            <span>{productReviewTotal}</span>
+            <span>
+              {productReviewTotal}
+            </span>
           </button>
-          <button type="button">
-            Reviews for this item
+          <button type="button" className="shop-review-button" onClick={() => this.setState({ selectedReviews: 'shop' })}>
+            Reviews for this shop
             <span>{shopReviewTotal}</span>
           </button>
         </div>
-        {/* Only show the first four */}
-        {/* Render data that will be added to reviewArray */}
-        {reviewArray.map(() => <Review />)}
-        {/* See More Button */}
-        {(reviewArray.length > 4) ? <button className="more-reviews-button" type="button">See More</button> : null}
+        {/* See More Reviews User story TODO: When see more reviews is clicked */}
+        {/* Up to 20 reviews are shown, if less than 20 it will show all of the reviews */}
+        {(selectedReviews === 'product') ? [
+          <Reviews getReviews={this.getFirstFourReviews} reviews={productReviews} />,
+          (productReviews.length > 4 && !seeMoreReviewsIsClicked)
+            ? <button className="more-reviews-button" type="button" onClick={() => this.moreReviewsClick()}>See More</button>
+            : null,
+          (seeMoreReviewsIsClicked)
+            ? <Reviews getReviews={this.getRestOfReviews} reviews={productReviews} /> : null,
+        ] : [
+          <Reviews getReviews={this.getFirstFourReviews} reviews={shopReviews} />,
+          (shopReviews.length > 4 && !seeMoreReviewsIsClicked) ? <button className="more-reviews-button" type="button" onClick={() => this.moreReviewsClick()}>See More</button> : null,
+          (seeMoreReviewsIsClicked)
+            ? <Reviews getReviews={this.getRestOfReviews} reviews={shopReviews} /> : null,
+        ]}
       </div>
     );
   }
 }
+// TODO: Refacor this line to put it in
+const Reviews = ({ getReviews, reviews }) => getReviews(reviews).map((data) => data);
 
 ReviewContainer.propTypes = {
   productReviewTotal: PropTypes.number,
