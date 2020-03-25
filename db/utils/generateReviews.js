99ecaefbe66407c639c4f030e63cfc4feccdf75b
Melvin-viana <mjviana057@gmail.com>
2020-03-25 11:43:09 -0700

    Change product reviews to add more realistic data for 1-9

--- db/utils/generateReviews.js
+++ db/utils/generateReviews.js
@@ -1,13 +1,19 @@
 const { lorem, random } = require('faker');
 
 const reviewData = [];
-const oneToOneHundred = () => random.number({ min: 1, max: 100 });
+let j = 0;
 for (let i = 0; i < 100; i += 1) {
+  if (i === 4) {
+    j += 1;
+  }
+  if (i % 10 === 0) {
+    j += 1;
+  }
   const currentReview = {};
   currentReview.body = lorem.paragraph();
   currentReview.score = random.number({ min: 1, max: 5 });
-  currentReview.ProductId = oneToOneHundred();
-  currentReview.ReviewerId = oneToOneHundred();
+  currentReview.ProductId = j;
+  currentReview.ReviewerId = i;
   reviewData.push(currentReview);
 }
 
